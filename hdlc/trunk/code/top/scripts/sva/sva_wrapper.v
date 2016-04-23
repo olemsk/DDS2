cf0c47b476fe7eda44d7f5cac213c6182ca0d0d8
olemsk <olemartinskafsaa@hotmail.com>
2016-04-23 14:37:59 +0200

    commit

--- hdlc/trunk/code/top/scripts/sva/sva_wrapper.v
+++ hdlc/trunk/code/top/scripts/sva/sva_wrapper.v
@@ -14,5 +14,6 @@ module sva_wrapper;
 		.abortframe(DUT.Tx_AbortFrame_D1),
 		.validframe(DUT.Tx_ValidFrame_D1),
 		.frame(DUT.TxCore.Frame_i) // Internal Frame strobe to flag insert block
+		.counter(DUT.WB_host.counter)
 );
 endmodule
