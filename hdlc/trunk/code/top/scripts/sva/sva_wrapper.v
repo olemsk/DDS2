d67363589f1012b3999c0d4a32fa778944f61a47
olemsk <olemartinskafsaa@hotmail.com>
2016-04-25 11:44:37 +0200

    Added sim_script.do in code folder to run the simmmeuerneAdded sim_script.do in code folder to run the simmmeuerne

--- hdlc/trunk/code/top/scripts/sva/sva_wrapper.v
+++ hdlc/trunk/code/top/scripts/sva/sva_wrapper.v
@@ -14,6 +14,5 @@ module sva_wrapper;
 		.abortframe(DUT.Tx_AbortFrame_D1),
 		.validframe(DUT.Tx_ValidFrame_D1),
 		.frame(DUT.TxCore.Frame_i) // Internal Frame strobe to flag insert block
-		.counter(DUT.WB_host.counter)
 );
 endmodule
