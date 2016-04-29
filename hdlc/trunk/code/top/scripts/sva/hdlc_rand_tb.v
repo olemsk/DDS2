379489b733dc43880ca93b70f61447e5733404f8
olemsk <olemartinskafsaa@hotmail.com>
2016-04-29 13:01:35 +0200

    hangala banhagala

--- hdlc/trunk/code/top/scripts/sva/hdlc_rand_tb.v
+++ hdlc/trunk/code/top/scripts/sva/hdlc_rand_tb.v
@@ -1,11 +1,35 @@
 module hdlc_rand_tb();
 
+////// Instantiate DUT //////
+
+//DUT hdlc_dut (
+
+////// Bind stufffff //////
+
+bind hdlc_ent hdlc_props hdlc_props_sva_bind(
+	.txclk(Txclk),
+	.rxclk(RxClk),
+	.tx(Tx),
+	.rx(Rx),
+	.txen(TxEN),
+	.rxen(RxEN),
+	.clk_i(CLK_I),
+	.dat_o(DAT_O),
+	.ack_o(ACK_O),
+	.txdone(TxDone),
+	.counter(WB_host.counter),
+	.abortframe(Tx_AbortFrame_D1),
+	.validframe(Tx_ValidFrame_D1),
+	.frame(TxCore.Frame_i) // Internal Frame strobe to flag insert block
+);
+
+
 typedef struct {
 	rand bit [7:0]start_flag;
-	rand bit [3:0]address;
+	rand bit [2:0]address;
 	rand bit [7:0]control;
 	rand byte 	  information[]; //?
-		  bit [7:0]fcs; // CRC?
+		  bit [15:0]fcs; // CRC?
 	rand bit [7:0]end_flag;
 } hdlc_frame_t;
 
@@ -19,7 +43,7 @@ class hdlc_frame;
 	endtask
 
 	task getbits(ref bit data_o, input int delay=1);
-		bit [x:0] header;
+		bit [18:0] header;
 		bit [x:0] tail;
 		header = {frame.start_flag, frame.address, frame.control};
 		tail = {frame.fcs, frame.end_flag};
@@ -31,8 +55,6 @@ class hdlc_frame;
 	endtask
 
 endclass: hdlc_frame
-
-
 
 
 ////// Testbench section //////
